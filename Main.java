b92dfdb7a3f7d132b36ace21e881ffe58fbac21c
MayarSultanAlanaze <126346292+MayarSultanAlanaze@users.noreply.github.com>
2023-05-11 14:14:26 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+public class Main {
+    public static void main(String[] args) {
+       //Practical Quiz 1
+
+
+      /* 1
+       Scanner s=new Scanner(System.in);
+        System.out.println(" please Enter the  number ");
+        int num= s.nextInt();
+
+            if (num %2==0){
+                System.out.println("The number is ever");
+            }else
+                System.out.println("the number is odd");*/
+
+
+        /* 2
+        Scanner s = new Scanner(System.in);
+        System.out.println("please enter the number ");
+        int num = s.nextInt();
+        int sum = num;
+        for (int i = 0; i < num; i++) {
+            s.nextInt();
+                System.out.println(" sum is number = " + sum);
+
+            } */
+
+           /* 3
+             int[] array= {10, -21 , 30, 31, -25, -30, -1};
+            System.out.println(array[0]+" is a positive number");
+            System.out.println(array[1]+" is a negative number");
+            System.out.println(array[2]+" is a positive number");
+            System.out.println(array[3]+" is a positive number");
+            System.out.println(array[4]+" is a negative number");
+            System.out.println(array[5] +" is a negative number");
+            System.out.println(array[6]+ " is a negative number");
+        }*/
+
+
+        }
+    }
+
+
+
